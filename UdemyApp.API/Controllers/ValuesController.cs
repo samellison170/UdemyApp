ae6292ff0ee05822a81aafd0b2ef26d17e2a7398
Sam <spevy3@mail.missouri.edu>
2020-02-18 21:38:46 -0600

    End of Section 3

--- UdemyApp.API/Controllers/ValuesController.cs
+++ UdemyApp.API/Controllers/ValuesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using UdemyApp.API.Data;
@@ -9,6 +10,7 @@ using UdemyApp.API.Data;
 namespace UdemyApp.API.Controllers
 {
     //http"//localhost:5000/api/values
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
@@ -19,7 +21,7 @@ namespace UdemyApp.API.Controllers
             _context = context;
         }
         // GET api/values
-        [HttpGet]
+    [HttpGet]
         public async Task<IActionResult> GetValues()
         {
             var values = await _context.Values.ToListAsync();
@@ -28,6 +30,7 @@ namespace UdemyApp.API.Controllers
         }
 
         // GET api/values/5
+        [AllowAnonymous]
         [HttpGet("{id}")]
         public async Task<IActionResult> GetValue(int id)
         {
---
+++ UdemyApp.API/Data/IAuthRepository.cs
@@ -0,0 +1,12 @@
+using System.Threading.Tasks;
+using UdemyApp.API.Models;
+
+namespace UdemyApp.API.Data
+{
+    public interface IAuthRepository
+    {
+         Task<User> Register(User user, string password);
+         Task<User> Login(string username, string password);
+         Task<bool> UserExists(string username);
+    }
+}
